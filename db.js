341c991fc167148c42721c66c6f3385530427aaf
SUAIIMO <suaiimo22@gmail.com>
2026-03-02 19:42:07 +0800

    fix env variable names

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-host: process.env.DB_HOST,
-user: process.env.DB_USER,
-password: process.env.DB_PASSWORD,
-database: process.env.DB_NAME,
-port: process.env.DB_PORT,
+host: process.env.MYSQLHOST,
+user: process.env.MYSQLUSER,
+password: process.env.MYSQLPASSWORD,
+database: process.env.MYSQL_DATABASE,
+port: process.env.MYSQLPORT,
 waitForConnections: true,
 connectionLimit: 10,
 queueLimit: 0,
