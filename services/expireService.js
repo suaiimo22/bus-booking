de786e33487646683695e86bdfe9cf73b3b179e9
SUAIIMO <suaiimo22@gmail.com>
2026-03-02 20:39:34 +0800

    fix expire service promise

--- services/expireService.js
+++ services/expireService.js
@@ -2,6 +2,7 @@ const db = require("../db");
 
 function startExpireJob() {
 setInterval(async () => {
+try {
 const now = new Date();
 
 const expireSql = `
@@ -11,17 +12,17 @@ WHERE status = 'PENDING'
 AND expired_at < ?
 `;
 
-db.query(expireSql, [now], (err, result) => {
-if (err) {
-console.log("Expire error:", err);
-}
+const [result] = await db.query(expireSql, [now]);
 
-if (result && result.affectedRows > 0) {
+if (result.affectedRows > 0) {
 console.log(result.affectedRows + " booking auto expired");
 }
-});
 
-}, 60000); // cek tiap 1 menit
+} catch (err) {
+console.error("Expire error:", err);
+}
+
+}, 60000);
 }
 
 module.exports = startExpireJob;
