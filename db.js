830eca1a63602fb572e1b32b7d0f182c599f9d5f
SUAIIMO <suaiimo22@gmail.com>
2026-03-03 14:51:55 +0800

    fix database env

--- db.js
+++ db.js
@@ -1,23 +1,14 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-host: process.env.MYSQLHOST || process.env.MYSQL_HOST,
-user: process.env.MYSQLUSER || process.env.MYSQL_USER,
-password: process.env.MYSQLPASSWORD || process.env.MYSQL_PASSWORD,
-database:
-process.env.MYSQLDATABASE ||
-process.env.MYSQL_DATABASE ||
-"railway",
-port: process.env.MYSQLPORT || process.env.MYSQL_PORT,
-ssl: {
-rejectUnauthorized: false
-},
+host: process.env.MYSQLHOST,
+user: process.env.MYSQLUSER,
+password: process.env.MYSQLPASSWORD,
+database: process.env.MYSQLDATABASE, // ⬅️ PENTING
+port: process.env.MYSQLPORT,
 waitForConnections: true,
-connectionLimit: 5,
-queueLimit: 0
+connectionLimit: 10,
+queueLimit: 0,
 });
-
-console.log("DB NAME:", process.env.MYSQLDATABASE);
-console.log("DB NAME ALT:", process.env.MYSQL_DATABASE);
 
 module.exports = db.promise();
